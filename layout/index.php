849fcef6d61a990539291e9490175943d13960b5
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-16 16:18:11 -0300

    Changed alts atributtes

--- layout/index.php
+++ layout/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="theme-color" content="#62a8ea">
-    <meta name="keywords"content="Hospital Ricardo Gutiérrez">
+    <meta name="keywords" content="Hospital Ricardo Gutiérrez">
     <meta name="description" content="Plataforma web para el Hospital Gutiérrez">
     <meta name="robots" content="index,follow">
     <meta property="og:url" content="https://grupo5.proyecto2017.linti.unlp.edu.ar/" />
@@ -27,28 +27,28 @@
     <header>
       <nav class="navbar">
           <div class="container">
-            <a class="brand" href="#" alt="Inicio" title="Inicio">
+            <a class="brand" href="#" title="Inicio">
               <img class="logo zoomin" src="assets/images/logo-2.png" alt="Logo" title="Logo">
               <p>Hospital Gutiérrez</p>
             </a>
             <div class="separator"></div>
             <div>
-              <select class="styled-select blue semi-square" alt="Menu de Administración" title=""Menu de Administración"">
+              <select class="styled-select blue semi-square" title="Menu de Administración">
                 <option selected disabled>Menu</option>
-                <option value="" alt="Menu de Usuarios" title="Menu de Usuarios">Usuarios</option>
-                <option value="" alt="Menu de Roles" title="Menu de Roles">Roles</option>
-                <option value="" alt="Menu de Permisos" title="Menu de Permisos">Permisos</option>
+                <option value="" title="Menu de Usuarios">Usuarios</option>
+                <option value="" title="Menu de Roles">Roles</option>
+                <option value="" title="Menu de Permisos">Permisos</option>
               </select>
             </div>
             <div class="nav-group">
               <form class="form-inline" action="#" method="GET">
                 <div class="input-group">
                   <i class="suffix material-icons">search</i>
-                  <input type="text" name="search" value="" alt="Introduzca su busqueda" title="Introduzca su busqueda" placeholder="Introduzca su busqueda">
+                  <input type="text" name="search" value="" title="Introduzca su busqueda" placeholder="Introduzca su busqueda">
                 </div>
               </form>
               <ul class="nav-items">
-                  <li><a href="./login" alt="Iniciar Sesión" title="Iniciar Sesión">Login</a></li>
+                  <li><a href="./login" title="Iniciar Sesión">Login</a></li>
               </ul>
             </div>
           </div>
