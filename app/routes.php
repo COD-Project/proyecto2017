3c9288be3a6e673de5415392542dba09292d37e5
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-28 12:34:24 -0300

    Preparing for frame debug

---
+++ app/routes.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Some examples of routes to make MVC applications
+ */
+$app->get('/', function() {
+    return "MBHFramework is working!";
+});
+$app->get('/:controller', function($controller) {
+    return "MBHFramework is working!";
+});
+$app->map(['GET', 'POST'], '/:controller/:method',
+            function($controller, $method) {
+                return "MBHFramework is working!";
+            });
+$app->map(['GET', 'POST'], '/:controller/:method/:data',
+            function($controller, $method, $data) {
+                return "MBHFramework is working!";
+            });
--- index.php
+++ index.php
@@ -2,3 +2,9 @@
 
 require __DIR__ . '/config/config.php';
 require __DIR__ . '/vendor/autoload.php';
+
+$app = new \Mbh\App;
+
+require __DIR__ . '/app/routes.php';
+
+$app->run();
