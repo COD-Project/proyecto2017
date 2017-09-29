a6a3960a24ed62c9323233518aefd8f001ea5e3d
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-28 21:18:25 -0300

    App working

--- app/config/routes.php
+++ app/config/routes.php
@@ -4,19 +4,19 @@
  * Routes to make MVC applications
  */
 $app->get('/', function() {
-    return "MBHFramework is working!";
+    return "app working";
 });
 
 $app->get('/:controller', function($controller) {
-    return "MBHFramework is working!";
+    return "app working";
 });
 
 $app->map(['GET', 'POST'], '/:controller/:method',
             function($controller, $method) {
-                return "MBHFramework is working!";
+                return "app working";
             });
 
 $app->map(['GET', 'POST'], '/:controller/:method/:data',
             function($controller, $method, $data) {
-                return "MBHFramework is working!";
+                return "app working";
             });
