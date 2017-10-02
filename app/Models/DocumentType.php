8105802464ae36fe30eb2dad78e08db8b9f51631
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-10-02 19:02:24 -0300

    Added Document Type model

---
+++ app/Models/DocumentType.php
@@ -0,0 +1,18 @@
+<?php namespace App\Models;
+
+/**
+ * created by Lucas Di Cunzolo
+ */
+
+class DocumentType extends \App\Model
+{
+    protected static $table = [
+        "name" => "tipos_documento",
+        "idColumn" => "id"
+    ];
+
+    protected static $columnData = [
+        "id" => "id",
+        "name" => "nombre"
+    ];
+}
