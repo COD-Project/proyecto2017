0925366d8d744a1f4dbcbe967971bf2b083c5eea
ulises-jeremias <ulisescf.24@gmail.com>
2017-10-02 18:09:42 -0300

    Fix

---
+++ app/Controller.php
@@ -0,0 +1,16 @@
+<?php namespace App;
+
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+
+/**
+ * created by Ulises J. Cornejo Fandos
+ */
+class Controller extends \Mbh\Controller
+{
+    public function __construct($app = null)
+    {
+        parent::__construct($app);
+        $this->template = new Twig_Environment(new Twig_Loader_Filesystem('./web/templates/'));
+    }
+}
