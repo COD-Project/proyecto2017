8658af1d7d33f901a39b6eda2bed695bcf276cee
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-03 19:02:18 -0300

    Created Rol model

---
+++ app/Models/Rol.php
@@ -0,0 +1,18 @@
+<?php namespace App\Models;
+
+/**
+ * created by Juan Cruz Ocampos
+ */
+
+class Permission extends \App\Model
+{
+    protected static $table = [
+        "name" => "roles",
+        "idColumn" => "id"
+    ];
+
+    protected static $columnData = [
+        "id" => "id",
+        "name" => "nombre"
+    ];
+}
