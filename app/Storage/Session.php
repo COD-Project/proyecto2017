2fe9c10439676d6b6b475b0199057e130aafeefe
ulises-jeremias <ulisescf.24@gmail.com>
2017-10-03 09:54:30 -0300

    Fix

--- app/Storage/Session.php
+++ app/Storage/Session.php
@@ -6,4 +6,24 @@
 class Session extends \Mbh\Storage\Session
 {
     use App\Traits\Connection;
+
+    const SESS_APP_ID = 'grupo5_app_id';
+
+    const SESSION_TIME = 18000;
+
+    /**
+     * Generates a session for a certain time for a user.
+     *
+     * @param int $id
+     *
+     * @return void
+     */
+    final public function generateSession($id)
+    {
+        $this->set(static::SESS_APP_ID, $id);
+        $e['session'] = time() + static::SESSION_TIME;
+        static::$db->update('usuarios', $e, "id='$id'", 'LIMIT 1');
+
+        return $this;
+    }
 }
