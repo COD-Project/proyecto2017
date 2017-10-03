0dec964bb01b497d267a041e1c8add99ce784153
ulises-jeremias <ulisescf.24@gmail.com>
2017-10-03 20:57:18 -0300

    Some changes

--- web/assets/javascripts/main.js
+++ web/assets/javascripts/main.js
@@ -1,1 +1,1 @@
-require('map.js');
+
--- web/assets/javascripts/map.js
+++ web/assets/javascripts/map.js
@@ -1,8 +1,1 @@
-const modules_root = "./../../../vendor/node_modules/";
 
-const mdb_js = modules_root + "mdbootstrap/js/";
-
-require(mdb + "jquery-3.2.1.min.js");
-require(mdb + "bootstrap.min.js");
-require(mdb + "mdb.min.js");
-require(mdb + "pooper.min.js");
