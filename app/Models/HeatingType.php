52aad78e795e60c706ea8e82607a5102049246fc
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-10-02 23:39:05 -0300

    Added Heating Type model

---
+++ app/Models/HeatingType.php
@@ -0,0 +1,18 @@
+<?php namespace App\Models;
+
+/**
+ * created by Lucas Di Cunzolo
+ */
+
+class HeatingType extends \App\Model
+{
+    protected static $table = [
+        "name" => "tipo_calefaccion",
+        "idColumn" => "id"
+    ];
+
+    protected static $columnData = [
+        "id" => "id",
+        "name" => "nombre"
+    ];
+}
