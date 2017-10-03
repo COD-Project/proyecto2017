459e1662c935ef011d6f95b6158f2cb4b12bdd2d
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-10-02 23:54:17 -0300

    Added Health Control model with methods

---
+++ app/Models/HealthControl.php
@@ -0,0 +1,45 @@
+<?php namespace App\Models;
+
+/**
+ * created by Lucas Di Cunzolo
+ */
+
+class HealthControl extends \App\Model
+{
+    protected static $table = [
+        "name" => "controles_de_salud",
+        "idColumn" => "id"
+    ];
+
+    protected static $columnData = [
+        "id" => "id",
+        "age" => "edad",
+        "date" => "fecha",
+        "weight" => "peso",
+        "completeVaccines" => "vacunas_completas",
+        "maturationChord" => "maduracion_acorde",
+        "exCommonPhysicist" => "ex_fisico_comun",
+        "exPhysicistObservations" => "ex_fisico_observaciones",
+        "pc" => "pc",
+        "ppc" => "ppc",
+        "height" => "talla",
+        "feeding" => "alimentacion",
+        "generalObservations" => "observaciones_generales",
+        "patientId" => "paciente_id",
+        "userId" => "user_id"
+    ];
+
+    function patient()
+    {
+        if ($this->patientId()) {
+            return Patient::find($this->patientId());
+        }
+    }
+
+    function user()
+    {
+        if ($this->userId()) {
+            return User::find($this->userId());
+        }
+    }
+}
