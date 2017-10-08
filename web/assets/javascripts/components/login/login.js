402bdf1142fe9f20e2ea91b39204c7e237c1e0dc
ulises-jeremias <ulisescf.24@gmail.com>
2017-10-07 22:44:59 -0300

    Fix

--- web/assets/javascripts/components/login/login.js
+++ web/assets/javascripts/components/login/login.js
@@ -21,7 +21,7 @@ class Login {
       let data = JSON.parse(response);
 
       $('#login_response').html(`
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <div class="alert alert-${data.success ? "success": "danger"} alert-dismissible fade show" role="alert">
           <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
           <strong></strong> ${data.message} </a>
         </div>
