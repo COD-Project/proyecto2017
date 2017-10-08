0618c11340a363d86e7766bf81aa465b8596c963
ulises-jeremias <ulisescf.24@gmail.com>
2017-10-07 22:42:41 -0300

    Some changes

--- web/assets/javascripts/components/login/login.js
+++ web/assets/javascripts/components/login/login.js
@@ -23,7 +23,7 @@ class Login {
       $('#login_response').html(`
         <div class="alert alert-success alert-dismissible fade show" role="alert">
           <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <strong></strong> ${data.message} </a>.
+          <strong></strong> ${data.message} </a>
         </div>
       `);
     });
