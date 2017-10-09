57561cc052711fe4d3a3d1dd0ad94cd73ca6a8b0
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-10-09 16:10:03 -0300

    Added Patients rules

--- app/Controllers/PatientsController.php
+++ app/Controllers/PatientsController.php
@@ -1,14 +1,38 @@
 <?php namespace App\Controllers;
 
+use App\Models\Patient;
+
 /**
  * created by Ulises Jeremias Cornejo Fandos
  */
-class HomeController extends \App\Controller
+
+class PatientsController extends \App\Controller
 {
     function __construct($app)
     {
         parent::__construct($app);
 
-        echo $this->template->render('patient/patient.twig');
+        $this->app->get('/patients', [ $this, 'render' ]);
+        $this->app->get('/patients/show/:id', [ $this, 'show' ]);
+
+        $this->app->router()->run();
+    }
+
+    public function render()
+    {
+        Patient::init();
+        $patients = Patient::all();
+        return $this->template->render('patients/patients.twig', [
+            'patients' => $patients
+        ]);
+    }
+
+    public function show($id)
+    {
+        Patient::init();
+        $patient = Patient::find($id);
+        return $this->template->render('patients/show.twig', [
+            'patient' => $patient
+        ]);
     }
 }
