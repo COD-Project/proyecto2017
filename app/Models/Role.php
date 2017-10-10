5aa80de4515b4f5716a9b101fd545fcfff90549f
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-09 22:34:44 -0300

    Added permissions method

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -16,8 +16,19 @@ class Role extends \App\Model
         "name" => "nombre"
     ];
 
-    public function permissions()
+    function permissions()
     {
-        
+        $select = "*";
+        $from = "rol_tiene_permisos";
+        $where =  "rol_id=" . $this->id();
+        $rolePermissions = static::$db->select($select, $from, $where);
+
+        Permission::init();
+
+        $permissions = array_map(function($rolePermission){
+          return Permission::find($rolePermission['permiso_id']);
+        }, $rolePermissions);
+
+        return $permissions;
     }
 }
