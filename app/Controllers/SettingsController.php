21cbbf169b7f470edaef59d8328eb240941f8b1f
ulises-jeremias <ulisescf.24@gmail.com>
2017-10-13 14:47:33 -0300

    Some update

--- app/Controllers/SettingsController.php
+++ app/Controllers/SettingsController.php
@@ -12,7 +12,22 @@ class SettingsController extends \App\Controller
         parent::__construct($app);
 
         $this->configFile = new File("uploads/config.json");
-
         $this->controllersFile = new File("uploads/controllers.json");
+
+        $this->app->get('/settings', [ $this, 'notFound' ]);
+        $this->app->get('/settings/:method', [ $this, 'notFound' ]);
+        $this->app->post('/settings/:method', function($controller, $method) {
+            if (!method_exists($controller, $method)) {
+                $controller->redirect("error/404");
+            }
+
+        }, [ $this ]);
+
+        $this->app->router()->run();
+    }
+
+    public function notFound()
+    {
+        $this->redirect("error/404");
     }
 }
