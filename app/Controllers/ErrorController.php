3d04910b85630d52862133b9d20148c44ec21239
ulises-jeremias <ulisescf.24@gmail.com>
2017-10-21 14:30:43 -0300

    Http status added - client errors update

--- app/Controllers/ErrorController.php
+++ app/Controllers/ErrorController.php
@@ -5,7 +5,9 @@
  */
 class ErrorController extends \App\Controller
 {
-    function __construct($app)
+    use \App\Traits\HttpError;
+
+    public function __construct($app)
     {
         parent::__construct($app);
 
@@ -16,18 +18,22 @@ class ErrorController extends \App\Controller
             'name' => APP_NAME
         ]);
 
-        $this->app->get('/error', function($template) {
+        $this->app->get('/error', function ($template) {
             return $template->render('error/404.twig');
         }, [$this->template]);
 
-        $this->app->get('/error/500', function($template, $session) {
+        $this->app->get('/error/500', function ($template, $session) {
+            header("HTTP/1.1 500 " . static::$http_status_codes[500]);
+
             return $template->render('error/500.twig', [
               'owner_user' => $session->sessionInUse()
             ]);
         }, [$this->template, $this->session]);
 
-        $this->app->get('/error/:code', function($template, $code) {
+        $this->app->get('/error/:code', function ($template, $code) {
             if (in_array($code, [404, 403, 301, 302])) {
+                header("HTTP/1.1 $code " . static::$http_status_codes[(int) $code]);
+
                 return $template->render("error/$code.twig");
             }
 
