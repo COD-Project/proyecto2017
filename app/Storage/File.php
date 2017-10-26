8a2e9a742fa9909396e21363c63ac4511e0036d2
ulises-jeremias <ulisescf.24@gmail.com>
2017-10-26 11:30:00 -0300

    Added methods and mutual exclusion on File class

--- app/Storage/File.php
+++ app/Storage/File.php
@@ -5,20 +5,20 @@
  */
 class File
 {
-    protected $file;
+    protected $path;
 
     public function __construct($file)
     {
-        $this->file = $file;
+        $this->path = $file;
     }
 
     public function content()
     {
         $lines = '';
-        $f = new \SplFileObject($this->file);
+        $file = new \SplFileObject($this->path);
 
         while (!$f->eof()) {
-            $lines .= $f->fgets();
+            $lines .= $file->fgets();
         }
 
         return (string) $lines;
@@ -26,7 +26,79 @@ class File
 
     public function write($content)
     {
-        $f = new \SplFileObject($this->file, 'w');
-        return (int) $f->fwrite($content);
+        $file = new \SplFileObject($this->path, 'w');
+
+        if (!$file->flock(LOCK_EX | LOCK_NB)) {
+            throw new \Exception("Error Locking File", 1);
+        }
+
+        $file->ftruncate(0);
+        $success = $file->fwrite($content);
+        $file->flock(LOCK_UN);
+
+        return $success;
+    }
+
+    /**
+     * Delete file
+     *
+     * @return bool true if success, false if does no exists
+     */
+    public function delete()
+    {
+        if (file_exists($this->path)) {
+            unlink($this->path);
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns the extension of any file, no matter if it is just the name or path with the name
+     *
+     *
+     * @return mixed string with the extension, returns an empty string if there is no information about the extension
+     */
+    public function getFileExt()
+    {
+        return pathinfo($this->path, PATHINFO_EXTENSION);
+    }
+
+    /**
+     * Returns the size in Kbytes of a file
+     *
+     * @return int
+     */
+    public function size()
+    {
+        return (int) round(filesize($this->path) * 0.0009765625, 1);
+    }
+
+    /**
+     * Returns the exact date and time of creation of a file
+     *
+     * @return string d-m-y h:i:s
+     */
+    public function creationDate()
+    {
+        return date('d-m-Y h:i:s', filemtime($this->path));
+    }
+
+    /**
+     * Creates a directory
+     *
+     * @param string
+     * @param int $permissions
+     *
+     * @return bool
+     */
+    public static function createDir($route, $permissions = 0755)
+    {
+        if (is_dir($route)) {
+            return false;
+        }
+
+        return (bool) mkdir($route, $permissions, true);
     }
 }
