a93f6cf7f80d195a4395df77c799470433d240e9
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-11-06 13:29:07 -0300

    Stop api loop

--- app/Api.php
+++ app/Api.php
@@ -15,11 +15,13 @@ class Api
             $bot = new \TelegramBot\Api\Client(self::API_TELEGRAM_TOKEN);
             $bot->command("turnos", function($message) use($bot){
                 $data = explode(" ", $message->getText());
-                $info = file_get_contents(URL . "turnos/{$data[1]}");
+                $ch = curl_init(URL . "turnos/{$data[1]}");
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $info = curl_exec($ch);
+                curl_close($ch);
 
                 $bot->sendMessage($message->getChat()->getId(), serialize($info));
             });
-            $bot->run();
         } catch (\TelegramBot\Api\Exception $e) {
             $e->getMessage();
         }
