cf241b160a2a8a44bb3612e7c9d557f42f4730dc
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-11-13 12:11:22 -0300

    Added header

--- app/Controller.php
+++ app/Controller.php
@@ -14,6 +14,7 @@ class Controller extends \Mbh\Controller
     {
         $className = "\\App\\Controllers\\" . ucwords($controller) . "Controller";
         if (class_exists($className)) {
+            header("HTTP/1.1 200 OK");
             return parent::create($className, $args);
         }
 
