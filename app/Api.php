763e7e7fda724ecba52105438601c97f6f69266b
ulises-jeremias <ulisescf.24@gmail.com>
2017-11-16 19:13:22 -0300

    Update Api.php

--- app/Api.php
+++ app/Api.php
@@ -20,7 +20,9 @@ class Api
                 $info = curl_exec($ch);
                 curl_close($ch);
 
-                $bot->sendMessage($message->getChat()->getId(), json_decode(json_encode($info))['message']);
+                $data = json_decode($info, true);
+                
+                $bot->sendMessage($message->getChat()->getId(), $data['message']);
             });
             $bot->run();
         } catch (\TelegramBot\Api\Exception $e) {
