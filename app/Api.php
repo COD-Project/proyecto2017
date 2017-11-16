944e5bc626b5697344d0138ae36cacd9385bd406
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-11-16 17:36:28 -0300

    bot running

--- app/Api.php
+++ app/Api.php
@@ -6,7 +6,7 @@
 
 class Api
 {
-    const API_TELEGRAM_TOKEN = "453106211:AAHHllrUxxiVp5sFEZqnx7TZnGw6RLQvdsg";
+    const API_TELEGRAM_TOKEN = "431938628:AAEMPLLdNleqotvUaHeZs8Oc66o2YMn8QWc";
     const API_BOTAN_TRACKER_TOKEN = "";
 
     public function __construct()
