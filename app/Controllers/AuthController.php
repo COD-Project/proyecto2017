6dce012261c6672bcb7eb59a4df9a346ebfa46ab
ulises-jeremias <ulisescf.24@gmail.com>
2017-11-16 18:29:30 -0300

    Update AuthController.php

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php namespace App\Controllers;
+
+use App\Api;
+
+/**
+ * @author Ulises Jeremias Cornejo Fandos
+ */
+class AuthController extends \App\Controller
+{
+    function __construct($app)
+    {
+        parent::__construct($app);
+
+        $this->app->get('/auth', function() {
+            return new Auth;
+        });
+        
+        $this->app->run();
+    }
+}
