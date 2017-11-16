961024ffda4ab2603d25ca5137d6175687bf0058
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-11-16 18:50:28 -0300

    Test

--- app/Api.php
+++ app/Api.php
@@ -20,7 +20,7 @@ class Api
                 $info = curl_exec($ch);
                 curl_close($ch);
 
-                $bot->sendMessage($message->getChat()->getId(), json_decode($info)["data"]);
+                $bot->sendMessage($message->getChat()->getId(), "ol");
             });
             $bot->run();
         } catch (\TelegramBot\Api\Exception $e) {
