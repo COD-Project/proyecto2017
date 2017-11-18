20faf466c8135e01acb04ec3137224c6679d7ffb
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-11-18 12:05:03 -0300

    Fix date format

--- app/Api.php
+++ app/Api.php
@@ -56,7 +56,9 @@ class Api
                 curl_close($ch);
 
                 $turns_time = array_map(function($date) {
-                    return "{$date["date"]} - {$date["time"]}";
+                    $date = (new \DateTime($date["date"]))->format("d-m-Y");
+                    $time = (new \DateTime($date["time"]))->format("H:i");
+                    return "$date - $time";
                 },json_decode($info, true)["data"]);
 
                 $response = "Turnos activos:\n\n" . join("\n", $turns_time);
@@ -75,7 +77,9 @@ class Api
                 curl_close($ch);
 
                 $turns_time = array_map(function($date) {
-                    return "{$date["date"]} - {$date["time"]}";
+                    $date = (new \DateTime($date["date"]))->format("d-m-Y");
+                    $time = (new \DateTime($date["time"]))->format("H:i");
+                    return "$date - $time";
                 },json_decode($info, true)["data"]);
 
                 $response = "Turnos activos para número de documento {$data[1]}:\n\n" . join("\n", $turns_time);
