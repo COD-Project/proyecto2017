6d79848f2d07c4c21c2447ffcb7b97f9ec1ca095
ulises-jeremias <ulisescf.24@gmail.com>
2017-11-20 07:21:49 -0300

    Update

--- app/Controllers/HealthcontrolsController.php
+++ app/Controllers/HealthcontrolsController.php
@@ -31,19 +31,16 @@ class HealthcontrolsController extends \App\Controller
     {
         $get = $this->get();
 
-        $users = User::get([
-          'name' => $username,
-          'active' => (int) ($active == 'active')
-      ]);
+        $healthcontrols = HealthControl::all();
 
         $pageNumber = !$get['page'] ? $get['page'] : $get['page'] - 1;
         $from = AMOUNT_PER_PAGE * (int) $pageNumber;
 
-        return $this->template->render('users/users.twig', [
-          'users' => $users ? array_slice($users, $from, AMOUNT_PER_PAGE) : [],
+        return $this->template->render('healthcontrols/healthcontrols.twig', [
+          'healthcontrols' => $healthcontrols ? array_slice($healthcontrols, $from, AMOUNT_PER_PAGE) : [],
           'page' => !$get['page'] ? 1 : $get['page'],
-          'last_page' => ceil(count($users) / AMOUNT_PER_PAGE),
-          'location' => "users/search/$active" . (!$username ? "" : "/$username")
+          'last_page' => ceil(count($healthcontrols) / AMOUNT_PER_PAGE),
+          'location' => "healthcontrols"
       ]);
     }
 
