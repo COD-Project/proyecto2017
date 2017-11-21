1e4f3d48d10ec705e89e054d38bd71ed85aed020
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-11-21 15:02:25 -0300

    Change order

--- app/Api.php
+++ app/Api.php
@@ -22,15 +22,15 @@ class Api
 
                 $data_curl = json_decode($info, true);
 
-                if ($data_curl["success"] == false) {
-                    $bot->sendMessage($message->getChat()->getId(), "error";
-                }
-
                 $turns_time = array_map(function($time) {
                     return "- $time";
                 }, json_decode($info, true)["data"]);
 
                 $response = "Turnos para la fecha {$date->format('d-m-Y')}:\n\n" . join("\n", $turns_time);
+
+                if ($data_curl["success"] == false) {
+                    $response = $data_curl["message"];
+                }
 
                 $bot->sendMessage($message->getChat()->getId(), $response);
             });
